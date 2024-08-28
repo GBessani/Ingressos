8006f17a96ad806968e40eee5087b083e2a1724d
gabriel bessani ferreira <gabriel_bessani@hotmail.com.br>
2024-08-27 21:49:17 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let pista = document.getElementById('qtd-pista')
+let superior = document.getElementById('qtd-superior')
+let inferior = document.getElementById('qtd-inferior')
+{   
+    let tipoDeIngresso = document.getElementById('tipo-ingresso').value;
+    let quantidade = document.getElementById('qtd').value;
+
+    let desconto = pista - quantidade
+
+}
